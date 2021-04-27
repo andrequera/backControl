c57b9927554f27d7023a93af87600c5a48692235
andrequera <andrequera@gmail.com>
2021-04-27 03:53:20 +0000

    creacion de modelado, conexion de la bd, visualizacion de las tablas

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Cliente, Cotizacion, Pedido, Inventario, Producto, Pedidos_Productos, Base
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,4 +13,10 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
 
     # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(Cliente, Base.session))
+    admin.add_view(ModelView(Cotizacion, Base.session))
+    admin.add_view(ModelView(Pedido, Base.session))
+    admin.add_view(ModelView(Inventario, Base.session))
+    admin.add_view(ModelView(Producto, Base.session))
+    admin.add_view(ModelView(Pedidos_Productos, Base.session))
+  
--- src/models.py
+++ src/models.py
@@ -1,19 +1,112 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+#from sqlalchemy import render_er
 
 db = SQLAlchemy()
-
+#Base = declarative_base()
+Base = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+            "password":self.password,
+    # do not serialize the password, its a security breach
+            }
+
+class Cliente(Base.Model):
+    #__tablename__ = 'cliente'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Base.Column(Base.Integer, primary_key=True)
+    nombre = Base.Column(Base.String(250), nullable=False)
+    empresa = Base.Column(Base.String(250), nullable=False)
+    email = Base.Column(Base.String(250), nullable=False)
+    telefono = Base.Column(Base.Integer)
+
+class Cotizacion(Base.Model):
+    #__tablename__ = 'cotizacion'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Base.Column(Base.Integer, primary_key=True)
+    producto = Base.Column(Base.String(250))
+    descripcion = Base.Column(Base.String(250))
+    paletas = Base.Column(Base.Integer, nullable=False)
+    botellas = Base.Column(Base.Integer)
+    sku= Base.Column(Base.String(250), nullable=False)
+    total = Base.Column(Base.Integer)
+    cliente_id = Base.Column(Base.Integer, ForeignKey('cliente.id'))
+    cliente = relationship(Cliente)
+
+class Pedido(Base.Model):
+    #@__tablename__ = 'pedido'
+    id = Base.Column(Base.Integer, primary_key=True)
+    nombre_cliente = Base.Column(Base.String(50))
+    fecha = Base.Column(DateTime,default=datetime.datetime.utcnow())
+    sku = Base.Column(Base.String(50))
+    producto = Base.Column(Base.String(50))
+    paleta = Base.Column(Base.Integer)
+    cantidad = Base.Column(Base.Integer)
+    cliente_id = Base.Column(Base.Integer, ForeignKey('cliente.id'))
+    cliente = relationship(Cliente)
+
+class Inventario(Base.Model):
+    #__tablename__='inventario'
+    id = Base.Column(Base.Integer, primary_key=True)
+    sku = Base.Column(Base.String(50))
+    producto = Base.Column(Base.String(50))
+    caja = Base.Column(Base.Integer)
+    cantidad = Base.Column(Base.Integer)
+    precio = Base.Column(Base.Integer)
+    fecha = Base.Column(DateTime,default=datetime.datetime.utcnow())
+
+class Producto(Base.Model):
+    #__tablename__='producto'
+    id= Base.Column(Base.Integer, primary_key=True)
+    nombre = Base.Column(Base.String(50))
+    descripcion = Base.Column(Base.String(50))
+    paleta = Base.Column(Base.Integer)
+    cantidad = Base.Column(Base.Integer)
+    sku = Base.Column(Base.String(50), ForeignKey ('inventario.sku'))
+    inventario = relationship(Inventario)
+
+class Pedidos_Productos(Base.Model):
+    #__tablename__='pedidos_productos'
+    id = Base.Column(Base.Integer, primary_key=True)
+    pedido_id = Base.Column(Base.Integer, ForeignKey('pedido.id'))
+    pedido = relationship(Pedido)
+    producto_id = Base.Column(Base.Integer, ForeignKey('producto.id'))
+    producto = relationship(Producto)
+
+
+
+
+
+#    def to_dict(self):
+#        return {}
+
+## Draw from SQLAlchemy base
+#render_er(Base, 'diagram.png')
+
+
+
+
+
+
+
+
+
+
+
